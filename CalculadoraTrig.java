ede7a6465586f882eca4758db86c86bdbaa8d8ea
liaaaaaa28 <jehand2014@gmail.com>
2026-05-29 22:46:44 -0600

    29 de mayo 3

--- CalculadoraTrig.java
+++ CalculadoraTrig.java
@@ -3,21 +3,28 @@ import java.util.Scanner;
 public class CalculadoraTrig {
     public static void main(String[] args) {
 //_______________________________________________________________________________________________________________________
-
-System.out.println("escoja la funcion que desea calcular: ");
-
-System.out.println("1. sin \n2. cos \n3. tan \n4. arcsin \n5. arcos \n6. arctan \n7. sinh \n8. cosh \n9. tanh");
+// Declaracion de variables
 
 Scanner Scanner = new Scanner(System.in); // Scanner para obtener la funcion que el usuario desee
 
+int opcion = -1;
+
+while (opcion != 0) {
+
+System.out.println("escoja la funcion que desea calcular: ");
+
+System.out.println("1. sin \n2. cos \n3. tan \n4. arcsin \n5. arcos \n6. arctan \n7. sinh \n8. cosh \n9. tanh \n0. salir");
+
+opcion = Scanner.nextInt();
+
+//_______________________________________________________________________________________________________________________
 // calcular el sin de un angulo 
-int opcion = Scanner.nextInt();
-if (opcion == 1) {
+if (opcion == 1) {;
     System.out.println("Ingrese el angulo en radianes: ");
     double angulo = Scanner.nextDouble();
     double resultado = Math.sin(angulo);
     System.out.println("El resultado de sin(" + angulo + ") es: " + resultado);
-    Scanner.close();
+  
 }
 //_______________________________________________________________________________________________________________________
 //calcular el cos de un angulo
@@ -37,6 +44,9 @@ else if (opcion == 3) {
     
 }
 //_______________________________________________________________________________________________________________________
+}// cierre del while
 
-}
-}
+Scanner.close();
+}// cierre del metodo main
+
+}// cierre de la clase CalculadoraTrig
