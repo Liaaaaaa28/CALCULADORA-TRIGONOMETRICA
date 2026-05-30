e158eaab4dcd9c8821d749743e07dac5099ff1ed
liaaaaaa28 <jehand2014@gmail.com>
2026-05-29 18:50:23 -0600

    29 de mayo 1

---
+++ CalculadoraTrig.java
@@ -0,0 +1,11 @@
+public class CalculadoraTrig {
+    public static void main(String[] args) {
+System.out.println("1. sin \n2. cos \n3. tan \n4. arcsin \n5. arcos \n6. arctan \n7. sinh \n8. cosh \n9. tanh");
+
+
+
+
+    }
+    
+}
+
